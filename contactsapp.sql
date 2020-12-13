cf71afd8aa5bf3a1104b96bc876936881afc8920
Dilshod Khudoyarov <DH.17@mail.ru>
2020-12-13 17:47:44 +0500

    Update contactsapp.sql

--- contactsapp.sql
+++ contactsapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Дек 12 2020 г., 22:01
+-- Время создания: Дек 13 2020 г., 15:46
 -- Версия сервера: 8.0.19
 -- Версия PHP: 7.4.5
 
@@ -29,10 +29,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `contacts` (
   `id` int UNSIGNED NOT NULL,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `number` bigint NOT NULL,
   `second_number` bigint DEFAULT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `second_email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -44,77 +44,10 @@ CREATE TABLE `contacts` (
 
 INSERT INTO `contacts` (`id`, `name`, `number`, `second_number`, `email`, `second_email`, `created_at`, `updated_at`) VALUES
 (1, 'Elon Musk', 998915556565, 998915556560, 'elonmusk@gmail.com', 'elonmusk2@gmail.com', '2020-12-10 11:31:49', '2020-12-10 11:31:54'),
-(2, 'Mark Zuckerberg', 998997551616, 998997551615, 'mark@gmail.com', 'mark2@gmail.com\r\n', '2020-12-10 11:32:01', '2020-12-10 11:32:05'),
-(3, 'Bill Gates', 998917855555, 998917855554, 'billgates@gmail.com', 'billgates2@gmail.com\r\n', '2020-12-10 11:32:10', '2020-12-10 11:32:14'),
-(4, 'Pavel Durov', 998995705565, 998995705564, 'durov@gmail.com', 'durov2@gmail.com\r\n', '2020-12-10 11:32:17', '2020-12-10 11:32:22'),
-(27, 'Dilshod Khudoyarov', 998915755555, NULL, 'blablabla@gmai.com', NULL, '2020-12-12 06:36:15', '2020-12-12 06:36:15');
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `failed_jobs`
---
-
-CREATE TABLE `failed_jobs` (
-  `id` bigint UNSIGNED NOT NULL,
-  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `migrations`
---
-
-CREATE TABLE `migrations` (
-  `id` int UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Дамп данных таблицы `migrations`
---
-
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2020_12_09_134940_contacts_table', 1);
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `password_resets`
---
-
-CREATE TABLE `password_resets` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `users`
---
-
-CREATE TABLE `users` (
-  `id` bigint UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+(39, 'Mark Zuckerberg', 998917455555, 998917455554, 'mark@gmail.com', 'mark2@gmail.com', '2020-12-13 06:42:33', '2020-12-13 06:42:33'),
+(40, 'Bill Gates', 998996552523, 998996552522, 'billgates@gmail.com', 'billgates2@gmail.com', '2020-12-13 06:43:58', '2020-12-13 06:43:58'),
+(41, 'Pavel Durov', 998914665352, 998914665351, 'durov@gmail.com', 'durov2@gmail.com', '2020-12-13 06:44:51', '2020-12-13 06:44:51'),
+(42, 'Dishkan Khudoyarov', 998998889999, NULL, 'blablabla@gmail.com', NULL, '2020-12-13 06:51:17', '2020-12-13 06:51:17');
 
 --
 -- Индексы сохранённых таблиц
@@ -130,32 +63,6 @@ ALTER TABLE `contacts`
   ADD UNIQUE KEY `name` (`name`);
 
 --
--- Индексы таблицы `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
-
---
--- Индексы таблицы `migrations`
---
-ALTER TABLE `migrations`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `password_resets`
---
-ALTER TABLE `password_resets`
-  ADD KEY `password_resets_email_index` (`email`);
-
---
--- Индексы таблицы `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `users_email_unique` (`email`);
-
---
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
@@ -163,25 +70,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `contacts`
 --
 ALTER TABLE `contacts`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
-
---
--- AUTO_INCREMENT для таблицы `failed_jobs`
---
-ALTER TABLE `failed_jobs`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `migrations`
---
-ALTER TABLE `migrations`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT для таблицы `users`
---
-ALTER TABLE `users`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
