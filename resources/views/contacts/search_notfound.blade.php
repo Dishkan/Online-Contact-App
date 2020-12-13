7f3b1e970acb309855fdef8cb4869ceda9955662
Dilshod Khudoyarov <DH.17@mail.ru>
2020-12-13 18:18:10 +0500

    Add files via upload

---
+++ resources/views/contacts/search_notfound.blade.php
@@ -0,0 +1,19 @@
+<div class="container">
+        <p> The search results for your query <b> {{ $query }} </b> are :</p>
+    <table class="table table-striped">
+        <tbody>
+            <tr>
+                <td>Nothing was found!</td>
+			</tr>
+			<tr>
+			  <td>
+                <a href="{{ route('main.index') }}">
+                    <button class="btn btn-success">
+                        <i class="fa fa-btn fa-create"></i> Back
+                    </button>
+				</a>
+			  </td>
+			</tr>
+        </tbody>
+    </table>  
+</div>
